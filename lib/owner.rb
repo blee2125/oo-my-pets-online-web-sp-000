dd4572971dfb013036cbad69bd64a9338b06ad3c
favorable-device-5158 <blee2125@gmail.com>
2019-09-25 20:12:05 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,30 +1,3 @@
 class Owner
-  attr_accessor
-  attr_reader :name, :species
-  attr_writer
-  @@all= []
-
-  def initialize(name, species= "human")
-    @name= name
-    @species= species
-    @@all << @name
-    @@all << @species
-  end
-
-  def say_species
-    return "I am a #{@species}."
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.count
-
-  end
-
-  def self.reset_all
-    
-  end
-
+  
 end
