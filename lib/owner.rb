577f6efc99ba70e2162afcd682831be309d66b2d
favorable-device-5158 <blee2125@gmail.com>
2019-09-25 20:15:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,13 @@
 class Owner
-  
+  attr_accessor
+  attr_reader :name, :species
+  attr_writer
+  @@all= []
+
+  def initialize(name)
+    @name= name
+    @species= "human"
+    @@all << self
+  end
+
 end
